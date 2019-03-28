ac486ef8a053e300a2181bce19331f7b7770a7a2
grelade <jacekgrela@gmail.com>
2019-03-28 00:49:46 +0000

    http -> https

--- server.js
+++ server.js
@@ -448,8 +448,8 @@ function handler(request, response) {
   request.on("end", function() {
 
     if (request.method == "GET") {
-      //console.log(urlObj.pathname)
-      if (urlObj.pathname=='/') urlObj.pathname = '/canvasWithTimer.html'
+      console.log(urlObj.pathname)
+      if (urlObj.pathname=='/') {urlObj.pathname = '/canvasWithTimer.html'}
       //handle GET requests as static file requests
       fs.readFile(ROOT_DIR + urlObj.pathname, function(err, data) {
         if (err) {
